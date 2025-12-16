0db626e6fdce1bb8db7653bca4ac286b740d731d
Rajagarwal632 <raj390504@gmail.com>
2025-12-17 00:32:54 +0530

    expense Schema defined

--- config/db.js
+++ config/db.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const { number, date } = require("zod")
 const { required } = require("zod/mini")
 const Schema = mongoose.Schema
 const objectid = Schema.ObjectId
@@ -8,11 +9,22 @@ mongoose.connect(process.env.MONGO_URL)
 const user = new Schema({
     email : {type : String , unique : true , required : true},
     password : {type : String , required : true},
-    name : String
+    name : {type : String , required : true}
+})
+
+const expense = new Schema({
+    userid : {type : objectid , required:true , ref : "user"},
+    amount : {type : number , required : true},
+    type : {type : String , enum : ["income" , "expense"]},
+    category : {type : String , required : true},
+    date : {type : date , required : true},
+    description : {type : String , required : true}
 })
 
 const usermodel = mongoose.model("user" , user)
+const expensemodel = mongoose.model("expense" , expense)
 
 module.exports = {
-    usermodel
+    usermodel,
+    expensemodel
 }
