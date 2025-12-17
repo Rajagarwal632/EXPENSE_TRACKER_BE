a5687e333cf3885d4f670b88de71810a3e13c334
Rajagarwal632 <raj390504@gmail.com>
2025-12-17 12:07:53 +0530

    /expense/update defined

--- route/expense.js
+++ route/expense.js
@@ -56,6 +56,52 @@ expenseroute.get("/",userauth,async function(req,res){
     })
 })
 
+expenseroute.put("/:id" , userauth , async function(req,res){
+    const userid = req.userid
+    const dataid = req.params.id
+    const reqbody = z.object({
+        amount : z.number().optional(),
+        category : z.string().optional(),
+        description : z.string().optional(),
+        date : z.string().optional(),
+        type : z.enum(["income" , "expense"]).optional()
+    })
+    const parsedatawithsucess = reqbody.safeParse(req.body)
+    if(!parsedatawithsucess.success){
+        res.json({
+            msg : "INCORRECT FORMAT",
+            error : parsedatawithsucess.error
+        })
+        return
+    }
+    const amount = req.body.amount
+    const category = req.body.category
+    const description = req.body.description
+    const date = req.body.date
+    const type = req.body.type
+
+    const update = await expensemodel.findOneAndUpdate({
+        userid : userid,
+        _id : dataid 
+    }, {
+        amount,
+        category,
+        description,
+        date,
+        type
+    })
+    if(update){
+        res.json({
+            msg : "DATA UPDATED"
+        })
+    }else{
+        res.json({
+            msg : "DATA NOT EXIST OR CREDENTIALS WRONG"
+        })
+    }
+
+})
+
 module.exports = {
     expenseroute
 }
